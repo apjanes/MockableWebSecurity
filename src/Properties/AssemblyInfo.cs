f4ecf4ad8b9b8a530ad31b0690a96811be868cae
Team City <teamcity@peppermint-it.com>
2011-12-28 22:32:31 +0000

    Updating version number

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Peppermint IT Limited")]
 [assembly: AssemblyProduct("Mockable Web Security")]
 [assembly: AssemblyCopyright("Copyright 2011 by Peppermint IT Limited")]
-[assembly: AssemblyVersion("0.0.0.158")]
+[assembly: AssemblyVersion("0.0.0.159")]
 
-[assembly: AssemblyFileVersion("0.0.0.158")]
+[assembly: AssemblyFileVersion("0.0.0.159")]
 
