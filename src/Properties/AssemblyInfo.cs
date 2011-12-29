a4cfae49e59b46d90109829aca83b99d60eb97c5
Team City <teamcity@peppermint-it.com>
2011-12-29 12:15:42 +0000

    Updating version number

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright 2011 by Peppermint IT Limited")]
 [assembly: AssemblyVersion("0.0.0.0")]
 
-[assembly: AssemblyFileVersion("0.0.0.328")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 
