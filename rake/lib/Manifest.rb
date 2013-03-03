81ef4c8231a6e57ecbcde5dbda8f70690c5db970
Team City <teamcity@peppermint-it.com>
2013-03-03 07:34:02 +0000

    Moving more assembly info stuff into the manifest.

--- rake/lib/Manifest.rb
+++ rake/lib/Manifest.rb
@@ -2,12 +2,27 @@ class Manifest
   require 'yaml'
   def initialize(path)
     @data = YAML.load_file(path)
+    self.solution = self.solution || "#{self.code}.sln"
+    self.title = self.title || self.name
   end
   
-  def solution
-    unless @data['solution']
-      @data['solution'] = "#{@data['code']}.sln"
+  def method_missing(meth, *args, &block)
+    meth = meth.to_s
+    if (!args.empty? and meth =~ /=$/)
+      meth = meth.chop
+      @data[meth] = args[0]
     end
-    @data['solution']
+    @data[meth]
+  end
+end
+
+class Hash
+  def method_missing(meth, *args, &block)
+    meth = meth.to_s
+    if (!args.empty? and meth =~ /=$/)
+      meth = meth.chop
+      self[meth] = args[0]
+    end
+    self[meth]
   end
 end
