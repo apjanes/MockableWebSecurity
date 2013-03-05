67a93df64eb9284fb419f11d130d151f5f82db17
Team City <teamcity@peppermint-it.com>
2013-03-05 06:43:11 +0000

    Updating version to: 0.0.2.46

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Mockable Web Security")]
-[assembly: AssemblyDescription("Many components of the System.Web.Security namespace are static methods that are difficult to mock or stub for testing. MockableWebSecurity wraps many of these classes in interfaces with proxy implementations allowing for more easy creation of testable code.")]
+[assembly: AssemblyDescription("Many components of the System.Web.Security namespace are static methods that are  difficult to mock or stub for testing. MockableWebSecurity wraps many of these  classes in interfaces with proxy implementations allowing for more easy creation  of testable code.")]
 [assembly: AssemblyCompany("Peppermint IT Limited")]
 [assembly: AssemblyProduct("Mockable Web Security")]
 [assembly: AssemblyCopyright("Copyright 2011 to 2012 by Peppermint IT Limited")]
 [assembly: AssemblyVersion("0.0.0.0")]
 
-[assembly: AssemblyFileVersion("0.0.2.45")]
+[assembly: AssemblyFileVersion("0.0.2.46")]
