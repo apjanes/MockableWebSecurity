8eeeb2ec0c4454117bce94228b537253b50f3112
Team City <teamcity@peppermint-it.com>
2013-03-05 06:33:59 +0000

    Adding copyright to manifest.

--- rake/lib/Manifest.rb
+++ rake/lib/Manifest.rb
@@ -1,9 +1,17 @@
 class Manifest
   require 'yaml'
+  require 'time'
   def initialize(path)
     @data = YAML.load_file(path)
     self.solution = self.solution || "#{self.code}.sln"
     self.title = self.title || self.name
+    
+    if self.copyright and self.copyright.start_year then
+      unless self.copyright.end_year then
+        self.copyright.end_year = Time.now.strftime('%Y')
+      end
+      self.copyright = "Copyright #{self.copyright.start_year} to #{self.copyright.end_year} by #{self.company}"
+    end
   end
   
   def method_missing(meth, *args, &block)
